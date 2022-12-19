f9f15d483215bb11d9372ec9eb289a7a1b77ac5b
payne-wu <wuzhipeng1289690157@gmail.com>
2022-12-19 20:44:15 +0800

    20221219204415

--- app/src/main/java/com/payne/ratelstarter/MainActivity.java
+++ app/src/main/java/com/payne/ratelstarter/MainActivity.java
@@ -1,1 +1,19 @@
-package com.payne.ratelstarter;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class MainActivity extends AppCompatActivity {  @Override  protected void onCreate(Bundle savedInstanceState) {    super.onCreate(savedInstanceState);    setContentView(R.layout.activity_main);  }}
+package com.payne.ratelstarter;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author payne-wu
+ * @date 20:30 2022/12/19
+ * @project RatelStarter
+ * @description
+ */
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
